c57c7078338244a000ac5cffbab64cb50af1a0a8
Gabriel André Guillén Pocón <guillengabriel010@gmail.com>
2026-09-23 19:01:10 -0600

    Renombrar Form1 a FrmPrincipal (EST-10)
    - Form1 pasa a FrmPrincipal (archivos .cs, .Designer.cs y .resx) y se actualizan
      Program.cs y el .csproj de Ejecucion_Navegador.
    - Se quita un using sin uso.

--- codigo/componentes/navegador/Ejecucion_Navegador/Ejecucion_Navegador/Program.cs
+++ codigo/componentes/navegador/Ejecucion_Navegador/Ejecucion_Navegador/Program.cs
@@ -24,7 +24,7 @@ namespace Ejecucion_Navegador
             ClsSesionPrueba.NavegadorMetIniciarSesionPrueba();
             // Fin cambio - Mario Alberto Taracena Pérez - 0901-23-9335
 
-            Application.Run(new Form1());
+            Application.Run(new FrmPrincipal());
         }
     }
 }
